e6523f155d9390177200c3b70835f2193813ad48
Randy Ridenour <>
2020-06-29 19:41:58 -0500

    Add Org-roam-bibtex.

--- packages.el
+++ packages.el
@@ -59,3 +59,11 @@
 (package! deft)
 (package! orgonomic
   :recipe (:host github :repo "aaronjensen/emacs-orgonomic"))
+(package! org-roam-bibtex
+  :recipe (:host github :repo "org-roam/org-roam-bibtex"))
+
+;; When using org-roam via the `+roam` flag
+(unpin! org-roam company-org-roam)
+
+;; When using bibtex-completion via the `biblio` module
+(unpin! bibtex-completion helm-bibtex ivy-bibtex)
