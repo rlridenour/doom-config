48b6fdfa1ebce482bccb41ef45f982556a4e14f8
Randy Ridenour <>
2020-11-23 09:24:23 -0600

    Add tab settings.

--- init.el
+++ init.el
@@ -106,8 +106,7 @@
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf               ; pdf enhancements
-       ;;prodigy           ; FIXME managing external services & code builders
+       ;; pdf               ; pdf enhancements ;;prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
